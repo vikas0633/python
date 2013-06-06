accb867b3e44a5cd47f6c7f001ec10f36dee77cd
vikas0633 <vikas0633@gmail.com>
2013-06-06 10:42:16 -0400

    20130606

--- 29a_MakeGeneWideTable.py
+++ 29a_MakeGeneWideTable.py
@@ -35,44 +35,69 @@ def logfile(infile):
 ### main argument to 
 
 def options(argv):
-    dir = ''
+    directory = ''
     suff= ''
+    gene_ID = ''
     try:
-        opts, args = getopt.getopt(argv,"hd:s:",["dir=","suff="])
+        opts, args = getopt.getopt(argv,"hd:s:g:",["dir=","suff=","gene_ID="])
     except getopt.GetoptError:
         print '''
-            python 29a_MakeGeneWideTable.py -d <dir> -s <suff>
+            python 29a_MakeGeneWideTable.py \
+                                            -d <dir> \
+                                            -s <suff> \
+                                            -g <gene_ID>
             '''
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print '''
-                python 29a_MakeGeneWideTable.py -d <dir> -s <suff>
+            python 29a_MakeGeneWideTable.py \
+                                            -d <dir> \
+                                            -s <suff> \
+                                            -g <gene_ID>
                 '''
             sys.exit()
         elif opt in ("-d", "--dir"):
-            dir = arg
+            directory = arg
         elif opt in ("-s", "--suff"):
             suff = arg
+        elif opt in ("-g", "--gene_ID"):
+            gene_ID = arg
+
             
-    logfile(dir)
+    logfile(directory)
             
-    return dir, suff
+    return directory, suff, gene_ID
     
                             
 ### Loop over the files in a folder
-def LoopFiles(dir, suff):
-    for root, dirs, files in os.walk(dir):
+def LoopFiles(direcotry, suff):
+    for root, dirs, files in os.walk(direcotry):
         for file in files:
             if file.endswith(suff):
-                print file
+                
+                process(file)
+                
+                
+### process the gene_ID information
+def GeneID(gene_ID):
+    gene_id_hash = {}
+    for line in open(gene_ID,'r'):
+        if len(line) > 1:
+            if line[0] != '#':
+                token = line.strip().split('\t')
+                gene_id_hash[token[4]] = "\t".join(token[0:4])
+    print len(gene_id_hash)
+    return gene_id_hash
 
 if __name__ == "__main__":
+    direcotry, suff, gene_ID = options(sys.argv[1:])
     
-    dir, suff = options(sys.argv[1:])
+    ### process the gene_ID information
+    gene_id_hash = GeneID(gene_ID) 
     
     ### loop over the files in the dir
-    LoopFiles(dir,suff)
+    LoopFiles(direcotry, suff)
     
     ### close the logfile
     o.close()
